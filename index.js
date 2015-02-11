5c681600967a499206223649e4a885b549c37fb8
William Blankenship <william.jblankenship@gmail.com>
2015-02-10 18:26:21 -0600

    Added endpoint for searching

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var ec = require('./elasticSearch')
+var models = require('./models')
+var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
+
 
 //Setup routes
 app.get("/",function(req,res) {
@@ -24,9 +27,23 @@ ec.init(function(e) {
   }
   console.log("Done!")
 
-  app.set('port', process.env.PORT || 5000)
+  app.set('port', process.env.PORT || 5001)
   //Start Server
   app.listen(app.get('port'),function() {
     console.log("Server running on port "+app.get('port'))
+  })
+})
+
+//Define routes
+
+app.use("/course/search",bodyParser.json())
+app.get("/course/search",function(req,res) {
+  var course = new models.course()
+  if(!course.set(req.body,{validate:true})) {
+    return res.status(400).json({e:course.validationError})
+  }
+  ec.course.search(course,function(e,result) {
+    if(e) return res.status(404).end()
+    res.json(result)
   })
 })
