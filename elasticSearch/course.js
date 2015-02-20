a236a3ea0e83bb2bc1b2011558bfe31e682ae82b
William Blankenship <william.jblankenship@gmail.com>
2015-02-20 15:48:00 -0600

    Allows multiple search params

--- elasticSearch/course.js
+++ elasticSearch/course.js
@@ -17,23 +17,32 @@ var async = require('async')
  *  all properties optional
  */
 m.search = function search(opts,cb) {
-  client.search({
-    index:'courses',
-    body: {
-      query : {
-        match: {
-          id: opts.get("id"),
-          university: opts.get("university"),
-          title: opts.get("title"),
-          department: opts.get("department"),
-          number: opts.get("number"),
-          section: opts.get("section"),
-          start: opts.get("start"),
-          end: opts.get("end")
-        }
-      }
-    }
-  },function(e,r) {
+  var search = {index:'courses',body:{query:{bool:{}}}}
+  var should = search.body.query.bool.should = []
+  if(opts.get('university'))
+    should.push({match:{university:opts.get('university')}})
+  if(opts.get('id'))
+    should.push({match:{id:opts.get('id')}})
+  if(opts.get('title'))
+    should.push({match:{title:opts.get('title')}})
+  if(opts.get('department'))
+    should.push({match:{department:opts.get('department')}})
+  if(opts.get('number'))
+    should.push({match:{number:opts.get('number')}})
+  if(opts.get('section'))
+    should.push({match:{section:opts.get('section')}})
+  if(opts.get('start'))
+    should.push({match:{start:opts.get('start')}})
+  if(opts.get('location'))
+    should.push({match:{location:opts.get('location')}})
+  if(opts.get('instructor'))
+    should.push({match:{instructor:opts.get('instructor')}})
+  if(opts.get('semester'))
+    should.push({match:{semester:opts.get('semester')}})
+  if(opts.get('end'))
+    should.push({match:{end:opts.get('end')}})
+  console.log(JSON.stringify(should))
+  client.search(search,function(e,r) {
     if(e) return cb(e)
     var result = []
     async.each(r.hits.hits,
