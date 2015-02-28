7da36ede5bf64817292e605386162e72c53d07e1
William Blankenship <william.jblankenship@gmail.com>
2015-02-28 01:04:31 -0600

    More cross origin stuff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var log = require('./logger')
 
 app.all("/",function(req,res,next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 })
 
